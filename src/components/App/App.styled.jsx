03655bbdccfcb8962ea89a04821124e4f339ca4e
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2023-08-21 16:33:13 +0200

    updated

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+
+export const Container = styled.div`
+  max-width: 600px;
+  min-height: 400px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 20px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 10px;
+  border: 1px solid black;
+`;
+
+export const Title = styled.h1`
+  text-align: center;  
+`;
+
+export const ContactListTitle = styled.h2`
+  text-align: center;
+  font-size: 28px;
+
+
+`
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import { useAuth } from 'hooks/useAuth';
+import { Icon, IconHome, LinkStyle, NavContainer } from './Navigation.styled';
+
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <NavContainer>
+      <LinkStyle to="/">
+        <IconHome /> Home
+      </LinkStyle>
+      {isLoggedIn && (
+        <LinkStyle to="/contacts">
+          <Icon /> Contacts
+        </LinkStyle>
+      )}
+    </NavContainer>
+  );
+};
